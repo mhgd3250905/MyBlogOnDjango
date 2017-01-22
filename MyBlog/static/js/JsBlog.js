678f1d6486520dcf15957a524524610c4a184698
ksheng <294851575@qq.com>
2017-01-22 13:13:35 +0800

    修改字体，并且修复blog列表点击跳转错误问题~ 加入了编辑界面提交跳转~

--- MyBlog/static/js/JsBlog.js
+++ MyBlog/static/js/JsBlog.js
@@ -11,15 +11,18 @@ function insertBlogs(blogs) {
                 var mTitle=$("<h2></h2>").text(blog.fields.title);
                 var mContent=$("<p></p>").addClass("mContent").text(blog.fields.shortContent);
                 var mTime=$("<p></p>").text(blog.fields.revisedTime);
-
+                var mId=$("<p></p>").addClass("blogId").text(blog.fields.revisedId);
+        
                 var mDivider=$("<div></div>").addClass("mDivider");
-                mDiv.append(mTitle).append(mContent).append(mTime);
+                mDiv.append(mTitle).append(mContent).append(mTime).append(mId);
                 mDiv.append(mDivider);
 
                 $('#myFrame').append(mDiv);
 
-                mTitle.click(function () {
-                    window.location.href="http://127.0.0.1:8000/blog/"+blog.fields.revisedId;
+                //点击blog区块的时候跳转到对应的blog中
+                mDiv.click(function () {
+                    // alert($(this).find("p").last().text());
+                    window.location.href="http://127.0.0.1:8000/blog/"+$(this).find("p").last().text();
                 })
         }
 }
